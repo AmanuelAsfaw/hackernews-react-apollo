b2dca0d3bd431552ec812477e6cdc790ef5cea5f
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-11-23 16:34:03 +0300

    Configure ApolloClient

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
