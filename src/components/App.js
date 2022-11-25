6c7b84d85607166fcd03738901714f82a39a5b34
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-11-25 09:49:28 +0300

    Create a Link with apollo mutation

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@
 // import '../styles/App.css';
 
 import React, { Component } from 'react';
+import CreateLink from './CreateLink.js';
 import LinkList from './LinkList.js';
 
 class App extends Component {
   render() {
-    return <LinkList />;
+    // return <LinkList />;
+    return <CreateLink />
   }
 }
 
