b983bb8a660f82a8c378822b13dad20d0ba9d64e
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-11-28 16:13:48 +0300

    Configer subscription using web-socket

--- src/index.js
+++ src/index.js
@@ -9,6 +9,20 @@ import { AUTH_TOKEN } from './constants';
 
 import { setContext } from '@apollo/client/link/context';
 
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect : true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+})
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
@@ -23,8 +37,20 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const link = split(
+  ({query}) => {
+    const {kind, operation} = getMainDefinition(query)
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    )
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
